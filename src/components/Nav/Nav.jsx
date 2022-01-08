b15ba2e9c4ec10bae8dbf08b6568982bd1fd5e9c
Tomasz <mandalorian99@gmail.com>
2022-01-08 16:22:16 +0000

    asdasd

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./Nav.scss";
 
 const Nav = () => {
+  const hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+  });
+
   return (
     <nav className="nav">
       <ol className="nav__links">
