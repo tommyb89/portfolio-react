bfbf668d59cf72dca95d7c7ea5768021eadecfe2
Tomasz <mandalorian99@gmail.com>
2022-01-10 00:02:27 +0000

    navigation styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -25,30 +25,28 @@ const Nav = () => {
         </label>
       </div>
 
-      {isActive && (
-        <ul className="nav__list">
-          <li className="nav__item">
-            <a className="nav__link" href="#top">
-              Home
-            </a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#work">
-              Work
-            </a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#experience">
-              About
-            </a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-      )}
+      <ul className={isActive ? "nav__list" : "nav__list show"}>
+        <li className="nav__item">
+          <a className="nav__link" href="#top">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link" href="#work">
+            Work
+          </a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link" href="#experience">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link" href="#contact">
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
