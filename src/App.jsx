f95fff1785366e174aff6d8f5dde6b9514087f46
Tomasz <mandalorian99@gmail.com>
2022-02-06 09:29:22 +0000

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Work from "./components/Work/Work";
 import { BrowserRouter as Router } from "react-router-dom";
-// Font Awesome
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Header />
         <Work />
+        <Experience />
       </div>
     </Router>
   );
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Experience.scss";
+
+const Experience = () => {
+  return (
+    <section className="experience" id="experience">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam maiores at
+      laboriosam culpa, repudiandae commodi magnam minima numquam dignissimos
+      qui quisquam voluptate, in cupiditate perferendis iusto atque consectetur,
+      vel obcaecati.
+    </section>
+  );
+};
+
+export default Experience;
